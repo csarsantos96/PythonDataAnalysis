f48d45bb8518af3cc658bb5aa4645866db8b6fec
Cesar Santos <csar.santos18@gmail.com>
2025-08-26 16:51:35 -0500

    feat: add some python files

---
+++ Math Stats for Data/applicationModels.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import joblib
+
+pd.set_option('display.width', None)
+pd.set_option('display.max_columns', None)
+
+df = pd.read_csv('clientes-v3-preparado.csv')
+
+print(df.isnull().sum().sum())
+
+# Carregar modelo treinado
+modelo_regressao_linear = joblib.load('modelo_regressao_linear.pkl')
+modelo_regressao_logistica = joblib.load('modelo_regressao_logistica.pkl')
+modelo_arvore_decisao = joblib.load('modelo_arvore_decisao.pkl')
+
+# Dados dos novos funcionários
+dados_novos_funcionarios = pd.DataFrame({
+    'idade':[35,45,30],
+    'anos_experiencia':[6,12,5],
+    'nivel_educacao_cod':[2,3,4], #Ensino Médio, Ensino Superior, Pós-graduação
+    'area_atuacao':[1,4,3] # Educação, Tecnologia, Saúde
+})
+
+# Prever o salário com modelo treinado
