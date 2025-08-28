6b2eda62bf716342cdd53d24321340ed2ff7f69d
Cesar Santos <csar.santos18@gmail.com>
2025-08-28 13:46:54 -0500

    feat:Finishing the Math Stats for data module

--- Math Stats for Data/applicationModels.py
+++ Math Stats for Data/applicationModels.py
@@ -18,7 +18,20 @@ dados_novos_funcionarios = pd.DataFrame({
     'idade':[35,45,30],
     'anos_experiencia':[6,12,5],
     'nivel_educacao_cod':[2,3,4], #Ensino Médio, Ensino Superior, Pós-graduação
-    'area_atuacao':[1,4,3] # Educação, Tecnologia, Saúde
+    'area_atuacao_cod':[1,4,3] # Educação, Tecnologia, Saúde
 })
 
 # Prever o salário com modelo treinado
+salario_previsto = modelo_regressao_linear.predict(dados_novos_funcionarios)
+
+for x in range(len(salario_previsto)):
+    print(f'Salário previsto do {x+1}º funcionario: R${salario_previsto[x]:,.2f}')
+
+#Classificar o salário com modelo treinado
+categoria_salario = modelo_regressao_logistica.predict(dados_novos_funcionarios)
+
+for x in range(len(categoria_salario)):
+    categoria = 'Acima da médiana' if categoria_salario[x] == 1 else 'Abaixo da mediana'
+    print(f'Catagoria de salário previsto do {x+1}º funcionario: {categoria}')
+
+    df = pd.read_csv('clientes-v2.csv')
